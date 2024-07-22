475b7456fe51c94610baa9f59612e3e455f5ecf7
Ota Fejfar <ota.fejfar@gmail.com>
2024-07-22 17:32:25 +0200

    feat: Adding the SPI Link Slave to the SPI channel processing
    Signed-off-by: Ota Fejfar <ota.fejfar@gmail.com>

---
+++ src/config_app.h
@@ -0,0 +1,13 @@
+
+#ifndef CONFIG_APP_H_
+#define CONFIG_APP_H_
+
+/******************************* HAL *******************************/
+
+    #define HAL_CFG_MCU     HAL_MCU_NRF52833      // Defined in the Makefile
+
+/****************************** Heap *******************************/
+
+    #define HEAP_SIZE       1024
+
+#endif /* CONFIG_APP_H_ */
